141f4ff17cba0969863926268954952ac12690e0
Malik Rafsanjani <pro.malikakbar2357@gmail.com>
2023-03-02 19:16:59 +0700

    feat: add original aes algo

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
+from aes import AES
+
 
 def main():
     print("halo")
+
+    msg = "rozan fadhil alhamdulillah"
+    aes = AES(0x2b7e151628aed2a6abf7158809cf4f3c)
+    digest = aes.encrypt_all_msg(msg)
+
+    print(digest)
+
+    decrypted = aes.decrypt_all_msg(digest)
+    print("decrypted", decrypted)
 
 
 if __name__ == "__main__":
