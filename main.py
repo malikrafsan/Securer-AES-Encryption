68e3eef2ff56c04997c10f29013df7c19baa6c0a
Malik Rafsanjani <pro.malikakbar2357@gmail.com>
2023-03-05 20:20:34 +0700

    feat: add intruder mocking experiment

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 from aes import AES
+import base64
 
 
 def main():
-    msg = "rozan fadhil alhamdulillah masih hidup"
+    # sender
+    msg = "MalikAkbarRafsanTidakKenalDenganMalikAkbarRafsanGan"
     key = "abcdefghij123456"
     aes = AES(key)
     digest = aes.encrypt(msg)
 
-    print("digest", digest)
-
+    # receiver
     decrypted = aes.decrypt(digest)
     print("decrypted", decrypted)
+
+    # intruder
+    print("digest", digest)
+    print("len(digest)", len(digest))
+    decoded = base64.b64decode(digest)
+    splitted = [decoded[i:i+16] for i in range(0, len(decoded), 16)]
+    print("splitted", splitted)
+    print("message block 1 == 3", splitted[0] == splitted[2])
 
 
 if __name__ == "__main__":
